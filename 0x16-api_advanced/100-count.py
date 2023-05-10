c1f407e0817dfe4f4e3bf4c3845e3776309d6f49
Florencecoker <damselpiccolo18@gmail.com>
2023-05-09 20:23:01 -0700

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,86 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""ALX SE API Module."""
+from operator import itemgetter
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
-
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
-
-    if after is None:
-        if len(instances) == 0:
-            print("")
+def recurse(subreddit, top_list=[]):
+    """Return list of title of the top posts in a subreddit."""
+    def recurse_post(lst, post_list, curr_idx):
+        """Recursively add all title to top_list."""
+        if curr_idx >= len(post_list):
             return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+        title = post_list[curr_idx]['data']['title']
+        lst.append(title)
+        recurse_post(lst, post_list, curr_idx + 1)
+
+    def get_post(limit, after, url, header, post_list):
+        """Recursively make request to reddit api(Handled its pagination)."""
+        if limit >= len(post_list):
+            return
+        url = '{}&after={}'.format(url, after)
+        response = requests.get(url, headers=header)
+        result = response.json()['data']
+        post_list = result['children']
+        after = result['after']
+        recurse_post(top_list, post_list, 0)
+        get_post(limit, after, url, header, post_list)
+
+    header = {
+            'User-Agent': 'MyBot/1.0'
+            }
+    limit = 100
+    url = 'https://reddit.com/r/{}/top.json?limit={}'.format(subreddit, limit)
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        result = response.json()['data']
+        post_list = result['children']
+        after = result['after']
+        recurse_post(top_list, post_list, 0)
+        get_post(limit, after, url, header, post_list)
+        return top_list
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        return None
+
+
+def count_words(subreddit, words_list):
+    """Print stats of word_list in the hot topics in a subreddit."""
+    def count_word(word, title, t_curr_idx, count=0):
+        """Return the number appear of word in title."""
+        if t_curr_idx >= len(title):
+            return count
+        if word == title[t_curr_idx]:
+            count += 1
+        return count_word(word, title, t_curr_idx + 1, count)
+
+    def get_stats(word_list, w_curr_idx, title_list, t_curr_idx, stats):
+        """Recursively get the stats."""
+        if t_curr_idx == len(title_list) - 1:
+            t_curr_idx = 0
+            w_curr_idx += 1
+        if w_curr_idx >= len(word_list):
+            return
+
+        word = word_list[w_curr_idx].lower()
+        title = title_list[t_curr_idx].lower()
+        count = count_word(word, title.split(), 0)
+        stats[word] = count + stats.get(word, 0)
+        get_stats(word_list, w_curr_idx, title_list, t_curr_idx + 1, stats)
+
+    def print_stats(stats, curr_idx):
+        """Recursively print the stats."""
+        if curr_idx >= len(stats):
+            return
+        k, v = stats[curr_idx]
+        if v > 0:
+            print('{}: {}'.format(k, v))
+        print_stats(stats, curr_idx + 1)
+
+    stats = {}
+    title_list = recurse(subreddit)
+    if not title_list:
+        return
+    get_stats(words_list, 0, title_list, 0, stats)
+    stats = sorted(stats.items(), key=lambda k: (
+        k[1], itemgetter(0)(k)), reverse=True)
+    print_stats(stats, 0)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns a list containing the
-titles of all hot articles for a given subreddit."""
+"""ALX SE API Module."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], n=0, after=None):
-    """ queries the Reddit API and returns a list containing the titles of
-    all hot articles for a given subreddit
+def recurse(subreddit, top_list=[]):
+    """Return list of title of the top posts in a subreddit."""
+    def recurse_post(lst, post_list, curr_idx):
+        """Recursively add all title to top_list."""
+        if curr_idx >= len(post_list):
+            return
+        title = post_list[curr_idx]['data']['title']
+        lst.append(title)
+        recurse_post(lst, post_list, curr_idx + 1)
 
-    The Reddit API uses pagination for separating pages of responses.
-    If not a valid subreddit, return None.
+    def get_post(limit, after, url, header, post_list):
+        """Recursively make request to reddit api(Handled its pagination)."""
+        if limit > len(post_list):
+            return
+        url = '{}&after={}'.format(url, after)
+        response = requests.get(url, headers=header)
+        post_list = response.json()['data']['children']
+        after = 't3_{}'.format(post_list[-1]['data']['id'])
+        recurse_post(top_list, post_list, 0)
+        get_post(limit, after, url, header, post_list)
 
-    Args:
-        subreddit (str): subreddit.
-        hot_list (list, optional): list of titles. Defaults to [].
-
-    Returns:
-        list: list of titles.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'custom'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        r = r.json()
-        for post in r.get('data').get('children'):
-            hot_list.append(post.get('data').get('title'))
-        if r.get('data').get('after'):
-            recurse(subreddit, hot_list)
-        return hot_list
+    header = {
+            'User-Agent': 'MyBot/1.0'
+            }
+    limit = 100
+    url = 'https://reddit.com/r/{}/top.json?limit={}'.format(subreddit, limit)
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        post_list = response.json()['data']['children']
+        after = 't3_{}'.format(post_list[-1]['data']['id'])
+        recurse_post(top_list, post_list, 0)
+        get_post(limit, after, url, header, post_list)
+        return top_list
     else:
         return None
